430fb6e95a35d363820d0596fba01a88393f995b
noykorleker <Noy.Korleker@s.afeka.ac.il>
2023-05-06 15:44:19 +0300

    block comment in personal information

--- lib/screens/personal_Information_screen.dart
+++ lib/screens/personal_Information_screen.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -32,4 +33,4 @@ class PersonalInformationScreen extends StatelessWidget {
       ),
     );
   }
-}
+}*/
