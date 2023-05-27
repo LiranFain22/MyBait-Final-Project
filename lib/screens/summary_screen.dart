7d8dac0bb45750795734124a2122e3b197047512
noykorleker <Noy.Korleker@s.afeka.ac.il>
2023-05-27 18:31:35 +0300

    summery screen version 2

--- lib/screens/summary_screen.dart
+++ lib/screens/summary_screen.dart
@@ -1,10 +1,15 @@
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:mybait/Services/firebase_helper.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import '../widgets/custom_toast.dart';
 
 class SummaryScreen extends StatelessWidget {
   static const routeName = '/summary';
-    SummaryScreen({super.key});
+
+  SummaryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -20,33 +25,30 @@ class SummaryScreen extends StatelessWidget {
             ),
             Column(
                 children: <Widget>[
-                  const Text(
-                    'payments: no payments yet!',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black87,
-                    ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(Icons.paid, color: Colors.green),
+                    title: Text("payments"),
+                    subtitle: Text("price"),
+                    trailing: const Icon(
+                      Icons.arrow_circle_right, color: Colors.lightBlueAccent),
                   ),
                   const Divider(),
-                  const Text(
-                    'reports: no reports yet!',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black87,
-                    ),
+                  ListTile(
+                    leading: const Icon(Icons.insert_chart, color: Colors.indigoAccent),
+                    title: Text("survey"),
+                    subtitle: Text("survey info"),
+                    trailing: const Icon(Icons.arrow_circle_right, color: Colors.lightBlueAccent),
                   ),
                   const Divider(),
-                  const Text(
-                    'suveys: no surveys yet!',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black87,
-                    ),
+                  ListTile(
+                    leading: const Icon(Icons.report, color: Colors.deepOrange),
+                    title: Text("report"),
+                    subtitle: Text("report info"),
+                    trailing: const Icon(Icons.arrow_circle_right, color: Colors.lightBlueAccent),
                   ),
-                ]),
+                ]
+            ),
           ],
         ),
       ),
